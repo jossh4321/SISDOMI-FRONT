dcd8ee28f82cc53c53a327bcc21ce16c86eb8e53
williammq <williammq@outlook.com>
2020-11-05 14:32:03 -0500

    RESIDENTE

--- src/components/residentes/DetalleResidente.vue
+++ src/components/residentes/DetalleResidente.vue
@@ -109,65 +109,23 @@
       <v-stepper-content step="2"><!--CONTIENE LOS STEPPERS 2 -->
         <div class="container-user">
       <form>
-        <v-text-field
-
-           v-model="residente.nombre"
-          label="Nombres:"
-          outlined
-          readonly
-          color="#009900"
-        ></v-text-field>
-        <v-text-field
-          v-model="residente.apellido"
-          label="Apellidos:"
-          outlined
-          readonly
-          color="#009900"
-        ></v-text-field>
-        <v-menu
-          v-model="datemenu"
-          :close-on-content-click="false"
-          :nudge-right="40"
-          transition="scale-transition"
-          offset-y
-          min-width="290px"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-              v-model="residente.fechanacimiento"
-              label="Fecha de Nacimiento"
-              prepend-icon="mdi-calendar"
-              readonly
-              v-bind="attrs"
-              v-on="on"
-              color="#009900"
-            ></v-text-field>
-          </template>
-          <v-date-picker
-            v-model="residente.fechanacimiento"
-            @input="menu2 = false"
-            locale="es-es"
-          ></v-date-picker>
-        </v-menu>
-        <v-select
-          :items="['DNI', 'Pasaporte', 'Carnet Extranjeria','dni']"
-          label="Tipo de Documento"
-          dense
-          outlined
-          readonly
-          color="#009900"
-           v-model="residente.tipodocumento"
-        ></v-select>
-        <v-text-field
-          v-model="residente.numerodocumento"
-          label="Ingrese el Numero de Documento"
-          outlined
-          readonly
-          color="#009900"
-        ></v-text-field>
-        <v-row>
+        <v-textarea
+        label="Motivo de Ingreso"
+        v-model="residente.motivoingreso"
+        disabled:true
+       > </v-textarea>
+  
+        <v-textarea
+        label="No se que campo es"
+        v-model="residente.motivoingreso"
+        disabled:true
+       > </v-textarea>
+        
+       
+     
+        <v-row> <!-- termina le texto -->
           <v-col>
-            <v-btn block @click="step = 2" color="success">
+            <v-btn block @click="step = 3" color="success">
               <v-icon left>mdi-page-next-outline</v-icon>
               <span>Continuar</span>
             </v-btn>
@@ -188,62 +146,29 @@
         <v-text-field
 
            v-model="residente.nombre"
-          label="Nombres:"
+          label="Modalidad:"
           outlined
           readonly
           color="#009900"
         ></v-text-field>
         <v-text-field
           v-model="residente.apellido"
-          label="Apellidos:"
+          label="Nivel:"
           outlined
           readonly
           color="#009900"
         ></v-text-field>
-        <v-menu
-          v-model="datemenu"
-          :close-on-content-click="false"
-          :nudge-right="40"
-          transition="scale-transition"
-          offset-y
-          min-width="290px"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-              v-model="residente.fechanacimiento"
-              label="Fecha de Nacimiento"
-              prepend-icon="mdi-calendar"
-              readonly
-              v-bind="attrs"
-              v-on="on"
-              color="#009900"
-            ></v-text-field>
-          </template>
-          <v-date-picker
-            v-model="residente.fechanacimiento"
-            @input="menu2 = false"
-            locale="es-es"
-          ></v-date-picker>
-        </v-menu>
-        <v-select
-          :items="['DNI', 'Pasaporte', 'Carnet Extranjeria','dni']"
-          label="Tipo de Documento"
-          dense
-          outlined
-          readonly
-          color="#009900"
-           v-model="residente.tipodocumento"
-        ></v-select>
         <v-text-field
-          v-model="residente.numerodocumento"
-          label="Ingrese el Numero de Documento"
+          v-model="residente.apellido"
+          label="Grado:"
           outlined
           readonly
           color="#009900"
         ></v-text-field>
+        
         <v-row>
           <v-col>
-            <v-btn block @click="step = 2" color="success">
+            <v-btn block @click="step = 1" color="success">
               <v-icon left>mdi-page-next-outline</v-icon>
               <span>Continuar</span>
             </v-btn>
