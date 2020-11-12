f8692cf2b887da52defe380090dd4b66bbedf231
Anderley Candela <anderleyc@gmail.com>
2020-11-12 14:14:12 -0500

    Finalizado anexos y firmas

--- src/components/informes/RegistrarInformeEducativoInicial.vue
+++ src/components/informes/RegistrarInformeEducativoInicial.vue
@@ -383,14 +383,7 @@
                       <v-icon left>mdi-close-outline</v-icon>
                       <span>Cerrar</span>
                     </v-btn>
-                  </v-col>
-                  <v-col>
-                    <v-btn block @click="sendPDFFiles()" color="primary">
-                      <v-icon left>mdi-close-outline</v-icon>
-                      <span>Testing</span>
-                    </v-btn>
-                  </v-col>
-
+                  </v-col>                  
                 </v-row>
               </form>
             </div>
@@ -493,9 +486,11 @@ export default {
           })
           .catch((err) => console.log(err));
       }
+      this.informe.contenido.anexos = listaanexos;
       console.log(listaanexos);
     },
     async registrarInforme() {
+      await this.sendPDFFiles();
       console.log(this.informe);
       this.$v.$touch();
       if (this.$v.$invalid) {
