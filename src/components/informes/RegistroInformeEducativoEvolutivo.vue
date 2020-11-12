11a590f19700d4028569717ad2a4822290fd6b8e
tp2_sea20192 <tp2_sea20192@hotmail.com>
2020-11-12 18:20:49 -0500

    Registro de Informes

--- src/components/informes/RegistroInformeEducativoEvolutivo.vue
+++ src/components/informes/RegistroInformeEducativoEvolutivo.vue
@@ -166,6 +166,34 @@
             ></v-text-field>
           </v-col>
         </v-row>
+        <v-menu
+                  v-model="datemenu"
+                  :close-on-content-click="false"
+                  :nudge-right="40"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="informe.fechacreacion"
+                      label="Fecha de Evaluación"
+                      prepend-icon="mdi-calendar"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                      color="#009900"
+                      @input="$v.informe.fechacreacion.$touch()"
+                      @blur="$v.informe.fechacreacion.$touch()"
+                      :error-messages="errorFechaEvaluacion"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker
+                    v-model="informe.fechacreacion"
+                    @input="menu2 = false"
+                    locale="es-es"
+                  ></v-date-picker>
+                </v-menu>
         <v-textarea
           v-model="informe.contenido.antecedentes"        
           label="Antecedentes"
@@ -473,7 +501,7 @@ import { required, minLength,email,helpers } from 'vuelidate/lib/validators'
 import moment from 'moment'
 
 export default {
-    name: "GestionarInformeEducativoEvolutivo",
+    name: "RegistrarInformeEducativoEvolutivo",
     props:["listaresidentes","visible","titulo","listaeducadores"],
     components: {
       vueDropzone: vue2Dropzone,
@@ -561,6 +589,12 @@ export default {
         async registrarInforme() {
           await this.sendPDFFiles();
           console.log(this.informe);
+          if(this.titulo === "Registrar Informe Educativo Evolutivo"){
+              this.informe.tipo = "InformeEducativoEvolutivo"
+          }else if(this.titulo === "Registrar Informe Educativo Final"){
+              this.informe.tipo = "InformeEducativoFinal"
+          }
+          console.log(this.informe);
           this.$v.$touch();
           if (this.$v.$invalid) {
             console.log("hay errores");
@@ -572,7 +606,6 @@ export default {
             );
           } else {
           console.log("no hay errores");
-          console.log(this.informe);
           await axios
             .post("/informe/informeee", this.informe)
             .then((res) => {
@@ -750,6 +783,19 @@ export default {
         !this.$v.informe.creadordocumento.required && errors.push("Debe seleccionar un educador obligatoriamente");
       return errors;
       },
+      errorFechaEvaluacion() {
+      const errors = [];
+      if (!this.$v.informe.fechacreacion.$dirty) return errors;
+      !this.$v.informe.fechacreacion.required &&
+        errors.push("Debe ingresar la fecha de evaluación obligatoriamente");
+      //validating whether the user are an adult
+      var dateselected = new Date(this.informe.fechacreacion);
+      var maxdate = new Date();
+      !(dateselected.getTime() < maxdate.getTime()) &&
+        errors.push("La fecha no debe ser mayor a la actual");
+
+      return errors;
+    },
       show: {
         get () {
           return this.visible
@@ -769,6 +815,9 @@ export default {
             },
             creadordocumento: {
               required
+            },
+            fechacreacion: {
+              required,
             },
            contenido: {
               antecedentes:{
