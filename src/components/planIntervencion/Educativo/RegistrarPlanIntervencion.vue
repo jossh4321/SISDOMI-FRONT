fcf08f6ad2897b95fe2ef85c412febd62db537d6
sebasmm12 <smirandamanrique@gmail.com>
2020-11-12 20:06:40 -0500

    Cambios ahhh

--- src/components/planIntervencion/Educativo/RegistrarPlanIntervencion.vue
+++ src/components/planIntervencion/Educativo/RegistrarPlanIntervencion.vue
@@ -335,7 +335,7 @@ export default {
         idresidente: "",
         fase: "",
         estado: "creado",
-        creadordocumento: "Piero Erickson Lavado Cervantes",
+        creadordocumento: "5f9e4cdae4655cf92eaa4d5b",
         contenido: {
           car: "",
           edad: 0,
