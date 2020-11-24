e685ed8cb7c1be03102ba0fdd7123925411e9715
Stefano GL <steph.gl@hotmail.com>
2020-11-24 12:19:26 -0500

    agregando firmas

--- src/components/sesioneseducativas/DetalleSesionEducativa.vue
+++ src/components/sesioneseducativas/DetalleSesionEducativa.vue
@@ -137,6 +137,153 @@
                             color="#009900"
                             label="Grado"
                         ></v-text-field>
+                        <v-menu
+                          v-model="datemenu2"
+                          :close-on-content-click="false"
+                          :nudge-right="40"
+                          transition="scale-transition"
+                          offset-y
+                          min-width="290px"
+                        >
+                          <template v-slot:activator="{ on, attrs }">
+                            <v-text-field
+                              v-model="item.fecha"
+                              style="margin-top_5px"
+                              :readonly="!edicion"
+                              color="#009900"
+                              prepend-icon="mdi-calendar"
+                              v-bind="attrs"
+                              v-on="on"
+                              label="Fecha de asistencia"
+                            ></v-text-field>
+                          </template>
+                          <v-date-picker
+                            v-model="item.fecha"
+                            :readonly="!edicion"
+                            @input="menu2 = false"
+                            locale="es-es"
+                          ></v-date-picker>
+                        </v-menu>
+                        <!-- empieza firmas -->
+                        <v-card
+                          style="margin-top:30px;padding:5px 5px;background-color:#EAEAEA"
+                        >
+                          <v-btn
+                            small
+                            dark
+                            color="green"
+                            @click="agregarFirma(item.idparticipante)"
+                          >
+                            <span v-if="item.firma!=''">Cambiar Firma</span>
+                            <span v-else>Añadir Firma</span>
+                          </v-btn>
+                          <v-row>
+                            <v-col :cols="12" align="right">
+                              <div>
+                                <vue-dropzone
+                                  ref="myVueDropzone"
+                                  @vdropzone-success="afterSuccess2"
+                                  @vdropzone-removed-file="afterRemoved2"
+                                  id="dropzone2"
+                                  :options="dropzoneOptions2"
+                                >
+                                </vue-dropzone>
+                              </div>
+                            </v-col>
+                          </v-row>
+                          <v-card
+                            color="#FAFAFA"
+                            style="margin-top:5px"
+                            height="60"
+                          >
+                            <v-row style="margin-left:10px;heigh:100%" align="center">
+                              <v-col :cols="8">
+                                <article>
+                                  <img
+                                    style="margin-right:5px;width:6% "
+                                    src="https://www.flaticon.es/svg/static/icons/svg/996/996443.svg"
+                                    alt="imagen usuario"
+                                  />
+                                  <span style="font-size:18px">
+                                    {{ item.datosresidente.nombre }} {{ item.datosresidente.apellido }}</span
+                                  >
+                                </article>
+                              </v-col>
+                              <v-col :cols="2" align="center">
+                                <template>
+                                    <v-btn
+                                      fab
+                                      icon=""
+                                      x-small
+                                      dark
+                                      color="#EAEAEA"
+                                      @click="verFirma(item.idparticipante)"
+                                    >
+                                      <img
+                                        style="width:25% "
+                                        src="https://www.flaticon.es/svg/static/icons/svg/1/1180.svg"
+                                        alt="firma"
+                                      />
+                                    </v-btn>
+                                  </template>
+                              </v-col>
+                              <v-col :cols="2" align="right">
+                                <div style="margin-right:20px">
+                                  <v-btn
+                                    fab
+                                    x-small
+                                    dark
+                                    color="red"
+                                    @click="eliminarFirma(item.idparticipante)"
+                                  >
+                                    <v-icon dark>
+                                      mdi-minus
+                                    </v-icon>
+                                  </v-btn>
+                                </div>
+                              </v-col>
+                            </v-row>
+                          </v-card>
+                        </v-card>
+                        <v-dialog
+                          v-model="dialogVistaPreviaFirma"
+                          persistent
+                          max-width="600px"
+                        >
+                          <v-card align="center">
+                            <v-card-title>
+                              <span class="headline">Vista previa</span>
+                            </v-card-title>
+                            <v-card-text>
+                              <img
+                                v-if="imagen.includes('http')"
+                                width="100%"
+                                height="100%"
+                                :src="imagen"
+                                alt=""
+                              />
+                              <img
+                                v-else
+                                width="100%"
+                                height="100%"
+                                :src="'data:image/jpeg;base64,' + imagen"
+                                alt=""
+                              />
+                            </v-card-text>
+                            <v-card-actions>
+                              <v-spacer></v-spacer>
+                              <v-btn
+                                color="blue darken-1"
+                                text
+                                @click="cerrarVistaPreviaFirma()"
+                              >
+                                Cerrar
+                              </v-btn>
+                            </v-card-actions>
+                          </v-card>
+                        </v-dialog>
+                        <!-- acaba firmas -->
+
                         <v-text-field
                             v-model="item.firma"
                             style="margin-top_5px"
@@ -198,11 +345,17 @@
 </template>
 
 <script>
+import vue2Dropzone from "vue2-dropzone";
+import "vue2-dropzone/dist/vue2Dropzone.min.css";
+import { required, minLength, email, helpers } from "vuelidate/lib/validators";
 import axios from "axios";
 import moment from "moment";
 export default {
   name: "DetalleSesionEducativa",
   props: ["sesioneducativa"],
+  components: {
+    vueDropzone: vue2Dropzone,
+  },
   data(){
     return{
       select: { value: "1", text: "Nombre" },
@@ -213,11 +366,43 @@ export default {
       ],
       participantesFiltrados: [],
       search:"",
+      urlfirma:"",
       edicion:false,
       botonGuardarSesionEducativa:false,
       step:1,
+      dialogVistaPreviaFirma:false,
+      imagen:"",
       datemenu: false,
-      datoSesion:{}
+      datemenu2: false,
+      datoSesion:{},
+      sesioneducativaMod:{
+        titulo:"",
+        idCreador:"",
+        fechaCreacion: new Date(),
+        area:"",
+        contenido:{
+          participantes:[]
+        },
+        tipo: "Sesion Educativa"
+      },
+      participanteMod:{
+        idparticipante:"",
+        grado:"",
+        fecha: new Date(),
+        firma:"",
+        observaciones:""
+      },
+      dropzoneOptions2: {
+        url: "https://httpbin.org/post",
+        thumbnailWidth: 250,
+        maxFilesize: 5.0,
+        maxFiles: 1,
+        acceptedFiles: ".png",
+        headers: { "My-Awesome-Header": "header value" },
+        addRemoveLinks: true,
+        dictDefaultMessage:
+          "Seleccione la imagen de la firma su dispositivo o arrástrela aquí",
+      },
     }
   },
   watch:{
@@ -269,6 +454,48 @@ export default {
       return array.length;
     },
 
+    agregarFirma(id) {
+      this.participantesFiltrados.forEach((part)=>{
+        if(part.idparticipante ===id){
+          part.firma= this.urlfirma;
+          //this.$refs.myVueDropzone.removeAllFiles();
+          this.urlfirma = "";
+        }
+      })
+    },
+    eliminarFirma(id) {
+      this.participantesFiltrados.forEach((part)=>{
+        if(part.idparticipante ===id){
+          part.firma= "";
+        }
+      })
+    },
+    verFirma(id) {
+      this.participantesFiltrados.forEach((part)=>{
+        if(part.idparticipante ===id){
+          console.log(part.firma);
+          console.log(part.firma);
+          console.log(part.firma);
+          console.log(part.firma);
+          this.imagen = part.firma;
+          console.log("imagen: "  + this.imagen);
+          this.dialogVistaPreviaFirma = true;
+        }
+      })
+    },
+    cerrarVistaPreviaFirma() {
+      this.dialogVistaPreviaFirma = false;
+    },
+    afterSuccess2(file, response) {
+      this.urlfirma = file.dataURL.split(",")[1];
+      console.log(this.urlfirma);
+      console.log("urlfirma llenada");
+    },
+    afterRemoved2(file, error, xhr) {
+      this.urlfirma = "";
+    },
+
+
     //Obtener datos de una sesión por id
     async obtenerSesionEducativaDTO(idsesion) {
       var user = {};
@@ -277,6 +504,9 @@ export default {
         .then((res) => {
           user = res.data;
           user.fechacreacion = user.fechacreacion.split("T")[0];
+          for (var x=0;x<user.contenido.participantes.length;x++){
+              user.contenido.participantes[x].fecha = user.contenido.participantes[x].fecha.split("T")[0];
+          }
         })
         .catch((err) => console.log(err));
       console.log("Sesion educativa DTO:");
@@ -295,6 +525,18 @@ export default {
 
 <style scoped>
 .resaltado{
-  font-weight: 600;
+  font-weight: 500;
+}
+.dropzone-custom-content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+.dropzone-custom-title {
+  margin-top: 0;
+  color: #00b782;
 }
 </style>
