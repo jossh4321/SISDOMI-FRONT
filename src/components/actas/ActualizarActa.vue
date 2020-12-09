e91978964667e9f6cd6d6ba7a8949e2f9b4a07b4
Alexis <201811484@urp.edu.pe>
2020-12-08 21:08:36 -0500

    actas

--- src/components/actas/ActualizarActa.vue
+++ src/components/actas/ActualizarActa.vue
@@ -1,6 +1,6 @@
 <template>
     <v-card>
-    <v-card-title class="justify-center">Registrar Acta de externamiento</v-card-title>
+    <v-card-title class="justify-center">Actualizar Acta de externamiento</v-card-title>
 
     <v-stepper v-model="step">
     <v-stepper-header>
