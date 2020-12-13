c4943217ebc3bcc791823e77d3ea4c8c16bb35a5
William <williammq@outlook.com>
2020-12-13 17:17:52 -0500

    Registrar,consultar,modificar  Ficha de de diagnostico educativo

---
+++ src/components/fichaDiagnosticoEducativo/ConsultarFichaDiagnosticoEducativo.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-card>
+    <v-card-title class="justify-center">Consultar Ficha Diagnostico Educativo</v-card-title>
+    <v-stepper v-model="step">
+      <v-stepper-header>
+        <v-stepper-step editable step="1">
+          Datos Generales
+        </v-stepper-step>
+
+        <v-divider></v-divider>
+
+        <v-stepper-step editable step="2">
+          Datos Especificos
+        </v-stepper-step>
+
+      </v-stepper-header>
+         <v-stepper-items>
+            <!--CONTIENE LOS STEPPERS CREADOS ARRIBA EN ESTE CASO SON TRES-->
+            <v-stepper-content step="1"
+              ><!--CONTIENE LOS STEPPERS 1 -->
+              <div class="container-user">
+                <form>
+
+
+                     <!--Botones de card -->
+                      <v-row>
+                        <v-col>
+                          <v-btn block @click="step = 2" color="success">
+                            <v-icon left>mdi-page-next-outline</v-icon>
+                            <span>Continuar</span>
+                          </v-btn>
+                        </v-col>
+                        <v-col>
+                          <v-btn block @click="cerrarDialogo()" color="primary">
+                            <v-icon left>mdi-close-outline</v-icon>
+                            <span>Cerrar</span>
+                          </v-btn>
+                        </v-col>
+                      </v-row> 
+                 </form>
+              </div>
+            </v-stepper-content>
+            <v-stepper-content step="2"
+              ><!--CONTIENE LOS STEPPERS 2 -->
+              <div class="container-user">
+                <form>
+
+
+                     <!--Botones de card -->
+                      <v-row>
+                        <v-col>
+                          <v-btn block @click="step = 1" color="success">
+                            <v-icon left>mdi-page-next-outline</v-icon>
+                            <span>Continuar</span>
+                          </v-btn>
+                        </v-col>
+                        <v-col>
+                          <v-btn block @click="cerrarDialogo()" color="primary">
+                            <v-icon left>mdi-close-outline</v-icon>
+                            <span>Cerrar</span>
+                          </v-btn>
+                        </v-col>
+                      </v-row> 
+                 </form>
+              </div>
+            </v-stepper-content>
+        </v-stepper-items>
+     </v-stepper>
+  </v-card>
+</template>
+
+<script>
+export default {
+name:"ConsultarFichaDiagnosticoEducativo",
+data(){
+    return{
+      step: 1,
+    }
+      },
+    methods:{
+      cerrarDialogo(){
+         this.step = 1;
+        this.$emit("close-dialog-save");
+       
+      },
+  }
+}
+</script>
+
+<style>
+
+</style>
