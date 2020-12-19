dae652f586ea881b081241f5fd0cc8fde9c34aeb
piero lavado cervantes <piero_lvdo_c@hotmail.com>
2020-12-18 21:15:28 -0500

    falto

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -59,27 +59,6 @@
                 <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" class="pa-4 mx-auto rounded-xl"></v-img>
 
                 <v-card-title class="justify-center">Tratamiento</v-card-title>
-
-                <!-- :elevation="hover ? 16 : 2"
-                  <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
-
-                <v-card-actions>
-                  <v-btn color="orange lighten-2" text>Explore</v-btn>
-
-                  <v-spacer></v-spacer>
-
-                  <v-btn icon @click="show = !show">
-                    <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-                  </v-btn>
-                </v-card-actions>
-
-                <v-expand-transition>
-                  <div v-show="show">
-                    <v-divider></v-divider>
-
-                    <v-card-text>I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.</v-card-text>
-                  </div>
-                </v-expand-transition> -->
               </v-card>
             </v-hover>
           </v-col>
@@ -99,27 +78,6 @@
                 <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" class="pa-4 mx-auto rounded-xl"></v-img>
 
                 <v-card-title class="justify-center">Tratamiento</v-card-title>
-
-                <!-- :elevation="hover ? 16 : 2"
-                  <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
-
-                <v-card-actions>
-                  <v-btn color="orange lighten-2" text>Explore</v-btn>
-
-                  <v-spacer></v-spacer>
-
-                  <v-btn icon @click="show = !show">
-                    <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-                  </v-btn>
-                </v-card-actions>
-
-                <v-expand-transition>
-                  <div v-show="show">
-                    <v-divider></v-divider>
-
-                    <v-card-text>I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.</v-card-text>
-                  </div>
-                </v-expand-transition> -->
               </v-card>
             </v-hover>
           </v-col>
