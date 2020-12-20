3b2eabb19a376debf6b912c5f373d9230894340c
josuec4321 <josuec4321@gmail.com>
2020-12-20 00:13:23 -0500

    vista principal del sistema

---
+++ src/components/usuarios/VisualizadorUsuario.vue
@@ -0,0 +1,110 @@
+<template lang="">
+    <v-expansion-panels  class="pa-4 mx-auto" max-width="90%">
+            <v-expansion-panel>
+              <v-expansion-panel-header disable-icon-rotate>
+                <v-row>
+                  <v-col xs="12" sm="6"   md="6" lg="3" xl="3">
+                    <v-card class="card-item">
+                      <span>Usuario: {{usuario.datos.nombre}} {{usuario.datos.apellido}}</span>
+                    </v-card>
+                  </v-col>
+                  <v-col xs="12" sm="6"   md="6" lg="3" xl="3">
+                    <v-card class="card-item">
+                      <span>{{usuario.datos.tipodocumento}}: {{usuario.datos.numerodocumento}}</span>
+                    </v-card>
+                  </v-col>
+                  <v-col xs="12" sm="6"   md="6" lg="3" xl="3">
+                    <v-card class="card-item">
+                      <span>Area : {{usuario.rol.area}}</span>
+                    </v-card>
+                  </v-col>
+                  <v-col xs="12" sm="6"   md="6" lg="3" xl="3">
+                    <v-card class="card-item">
+                      <span>Cargo Actual : {{usuario.rol.nombre}}</span>
+                    </v-card>
+                  </v-col>
+                </v-row>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-row>
+                  <v-col xs="12" sm="6"   md="6" lg="3" xl="3">
+                    <v-card class="card-item">
+                      <span>Direccion: {{usuario.datos.direccion}} {{usuario.datos.apellido}}</span>
+                    </v-card>
+                  </v-col>
+                  <v-col xs="12" sm="6"   md="6" lg="3" xl="3">
+                    <v-card class="card-item">
+                      <span>E-mail: {{usuario.datos.email}}</span>
+                    </v-card>
+                  </v-col>
+                  <v-col xs="12" sm="6"   md="6" lg="3" xl="3">
+                    <v-card class="card-item">
+                      <span>Fecha Nac.: {{usuario.datos.fechanacimiento | fomatoFecha}}</span>
+                    </v-card>
+                  </v-col>
+                  <v-col xs="12" sm="6"   md="6" lg="3" xl="3">
+                    <v-card class="card-item">
+                      <span>Estado Actual : {{usuario.estado}}</span>
+                    </v-card>
+                  </v-col>
+                   <v-col xs="12" sm="12"   md="12" lg="12" xl="12">
+                    <v-card class="card-item">
+                      <v-sheet
+                            class="mx-auto"
+                            max-width="90%"
+                        >
+                            <v-slide-group
+                            multiple
+                            show-arrows
+                            >
+                            <v-slide-item
+                                v-for="permiso in usuario.rol.permisos"
+                                :key="permiso.id"
+                                v-slot="{ active, toggle }"
+                            >
+                                <v-btn
+                                class="mx-2"
+                                :input-value="active"
+                                active-class="purple white--text"
+                                depressed
+                                rounded
+                                @click="toggle"
+                                >
+                                <v-icon left dark>
+                                    mdi-cloud-upload
+                                </v-icon>
+                                    {{permiso.nombre}}
+                                </v-btn>
+                            </v-slide-item>
+                            </v-slide-group>
+                        </v-sheet>
+                    </v-card>
+                  </v-col>
+                </v-row>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+</template>
+<script>
+import moment from "moment";
+export default {
+    props:["usuario"],
+    data(){
+            return {
+
+            }
+    },filters:{
+        fomatoFecha: (fecha) =>{
+            var formato = moment(fecha);
+            return formato.format("llll");
+        }
+    }
+}
+</script>
+<style scoped>
+    .card-item{
+        border: 2.5px solid #bbb;
+        border-radius: 5px;
+        padding: 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,83 +1,49 @@
 <template>
   <div>
-    <v-card class="card">
-      <v-card-title>BIENVENIDO A SISCAR</v-card-title>
+    <v-card class="card pa-4 mx-auto">
+      <v-card-title class="justify-center" style="font-size: 35px"
+        >BIENVENIDO A SISCAR</v-card-title
+      >
       <v-container>
+        <v-row v-if="user != undefined">
+          <VisualizadorUsuario :usuario="user"></VisualizadorUsuario>
+        </v-row>
         <v-row>
-          <v-col>
-            <v-hover
-              v-slot="{ hover }"
-            >
-              <v-card 
-                class="pa-4 mx-auto rounded-xl"
+          <v-col
+            v-for="opcion in listaOpciones"
+            :key="opcion.id"
+            xs="12"
+            sm="6"
+            md="6"
+            lg="4"
+            xl="4"
+          >
+            <v-hover v-slot="{ hover }">
+              <v-card
+                class="pa-4 mx-auto"
                 :class="{ 'on-hover': hover }"
-                max-width="300" 
-                @click.native="abrirVista()" 
-                style="cursor: pointer;"
-                rounded-pill
-                >
-                <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" class="pa-4 mx-auto rounded-xl"></v-img>
+                max-width="90%"
+                @click.native="imprimir()"
+                style="cursor: pointer"
+              >
+                <v-img
+                  :src="opcion.imagenUrl"
+                  height="200px"
+                  class="pa-4 mx-auto"
+                ></v-img>
 
-                <v-card-title class="justify-center">Tratamiento</v-card-title>
-
-                <!-- :elevation="hover ? 16 : 2"
-                  <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
-
-                <v-card-actions>
-                  <v-btn color="orange lighten-2" text>Explore</v-btn>
-
-                  <v-spacer></v-spacer>
-
-                  <v-btn icon @click="show = !show">
-                    <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-                  </v-btn>
-                </v-card-actions>
-
-                <v-expand-transition>
-                  <div v-show="show">
-                    <v-divider></v-divider>
-
-                    <v-card-text>I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.</v-card-text>
-                  </div>
-                </v-expand-transition> -->
-              </v-card>
-            </v-hover>
-          </v-col>
-
-          <v-col>
-            <v-hover
-              v-slot="{ hover }"
-            >
-              <v-card 
-                class="pa-4 mx-auto rounded-xl"
-                :class="{ 'on-hover': hover }"
-                max-width="300" 
-                @click.native="abrirVista()" 
-                style="cursor: pointer;"
-                rounded-pill
-                >
-                <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" class="pa-4 mx-auto rounded-xl"></v-img>
-
-                <v-card-title class="justify-center">Tratamiento</v-card-title>
-              </v-card>
-            </v-hover>
-          </v-col>
-
-          <v-col>
-            <v-hover
-              v-slot="{ hover }"
-            >
-              <v-card 
-                class="pa-4 mx-auto rounded-xl"
-                :class="{ 'on-hover': hover }"
-                max-width="300" 
-                @click.native="abrirVista()" 
-                style="cursor: pointer;"
-                rounded-pill
-                >
-                <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" class="pa-4 mx-auto rounded-xl"></v-img>
-
-                <v-card-title class="justify-center">Tratamiento</v-card-title>
+                <v-card-title class="justify-center">{{
+                  opcion.titulo
+                }}</v-card-title>
+                <v-card-text style="text-align: justify">
+                  {{ opcion.descripcion }}
+                </v-card-text>
+                <v-btn block class="my-button" color="success">
+                  <v-icon left dark @click.native="imprimir()">
+                    {{ opcion.boton.icono }}
+                  </v-icon>
+                  {{ opcion.boton.texto }}
+                </v-btn>
               </v-card>
             </v-hover>
           </v-col>
@@ -89,18 +55,89 @@
 
 <script>
 // @ is an alias to /src
+import VisualizadorUsuario from "@/components/usuarios/VisualizadorUsuario.vue"
 import HelloWorld from "@/components/HelloWorld.vue";
-
+import {mapGetters} from "vuex";
 export default {
   name: "Home",
+  data() {
+    return {
+      listaOpciones: [
+        {
+          id: 1,
+          imagenUrl: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
+          titulo: "Tratamiento Integral",
+          descripcion:
+            "Interfaz de Gestion especializada en el control, seguimineto , investigacion y registro de datos relacionados con los usuarios del CAR",
+          interfazUrl: "/dashboard/Tratamiento",
+          boton: { texto: "Realizar Tratamiento", icono: "mdi-cloud-upload" },
+        },
+        {
+          id: 2,
+          imagenUrl: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
+          titulo: "Tratamiento Integral",
+          descripcion:
+            "Interfaz de Gestion especializada en el control, seguimineto , investigacion y registro de datos relacionados con los usuarios del CAR",
+          interfazUrl: "/dashboard/Tratamiento",
+          boton: { texto: "Realizar Tratamiento", icono: "mdi-cloud-upload" },
+        },
+        {
+          id: 3,
+          imagenUrl: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
+          titulo: "Tratamiento Integral",
+          descripcion:
+            "Interfaz de Gestion especializada en el control, seguimineto , investigacion y registro de datos relacionados con los usuarios del CAR",
+          interfazUrl: "/dashboard/Tratamiento",
+          boton: { texto: "Realizar Tratamiento", icono: "mdi-cloud-upload" },
+        },
+        {
+          id: 4,
+          imagenUrl: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
+          titulo: "Tratamiento Integral",
+          descripcion:
+            "Interfaz de Gestion especializada en el control, seguimineto , investigacion y registro de datos relacionados con los usuarios del CAR",
+          interfazUrl: "/dashboard/Tratamiento",
+          boton: { texto: "Realizar Tratamiento", icono: "mdi-cloud-upload" },
+        },
+        {
+          id: 5,
+          imagenUrl: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
+          titulo: "Tratamiento Integral",
+          descripcion:
+            "Interfaz de Gestion especializada en el control, seguimineto , investigacion y registro de datos relacionados con los usuarios del CAR",
+          interfazUrl: "/dashboard/Tratamiento",
+          boton: { texto: "Realizar Tratamiento", icono: "mdi-cloud-upload" },
+        },
+        {
+          id: 6,
+          imagenUrl: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
+          titulo: "Tratamiento Integral",
+          descripcion:
+            "Interfaz de Gestion especializada en el control, seguimineto , investigacion y registro de datos relacionados con los usuarios del CAR",
+          interfazUrl: "/dashboard/Tratamiento",
+          boton: { texto: "Realizar Tratamiento", icono: "mdi-cloud-upload" },
+        },
+      ],
+    };
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    VisualizadorUsuario
+  },
+  created(){
+    
+  },
+  computed:{
+       ...mapGetters(["user"]),
   },
   methods: {
-    abrirVista() {
-      this.$router.push('/dashboard/Tratamiento')
+     imprimir(){
+        console.log(this.user);
+     },
+    abrirVista(urlInterfaz) {
+      this.$router.push(urlInterfaz);
     },
-  }
+  },
 };
 </script>
 
@@ -111,20 +148,26 @@ export default {
 .card {
   margin: 20px;
 }
-.on-hover{
+.my-button {
+  border-radius: 5px 5px 5px 5px;
+  font-size: 12px;
+}
+.on-hover {
   background-color: white;
 }
 .on-hover:hover {
-  box-shadow: 20px 20px 40px 0px rgba(0, 0, 0, 0.5);
+  box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.5);
   transition: height 0.3s, box-shadow 0.3s;
   transform-origin: center center;
-  animation: scale 500ms ease-in-out forwards;
-  background-color: #ff695b;
-  transition: background-color 500ms ease-in-out;
+  animation: scale 300ms ease-in-out forwards;
+  background-color: #d3f1ec;
+  transition: background-color 300ms ease-in-out;
 }
+
+
 @keyframes scale {
   to {
-    transform: scale(1.2);
+    transform: scale(1.01);
   }
 }
 </style>
